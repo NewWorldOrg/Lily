63a59b3fdd99e435f6f78ef406dcddaefa02154f
fyui001 <yun.cod@gmail.com>
2024-11-17 04:01:51 +0900

    fix query log provider

--- app/Providers/DataBaseQueryLoggerServiceProvider.php
+++ app/Providers/DataBaseQueryLoggerServiceProvider.php
@@ -6,6 +6,9 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use DateTime;
+use DateTimeZone;
+use DB;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
 
 class DataBaseQueryLoggerServiceProvider extends ServiceProvider
@@ -27,34 +30,42 @@ class DataBaseQueryLoggerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->environment("local")) {
-            \DB::listen(function ($query) {
-                $route = 'NOT URL';
-                if (method_exists(new \Request(), 'getName')) {
-                    $route = \Request::route()->getName();
-                }
-                $sql = $query->sql;
-                foreach ($query->bindings as $binding) {
-                    if (is_string($binding)) {
-                        $binding = "'{$binding}'";
-                    } elseif (is_bool($binding)) {
-                        $binding = $binding ? '1' : '0';
-                    } elseif (is_int($binding)) {
-                        $binding = (string) $binding;
-                    } elseif ($binding === null) {
-                        $binding = 'NULL';
-                    } elseif ($binding instanceof Carbon) {
-                        $binding = "'{$binding->toDateTimeString()}'";
-                    } elseif ($binding instanceof DateTime) {
-                        $binding = "'{$binding->format('Y-m-d H:i:s')}'";
-                    }
-                    $sql = preg_replace('/\\?/', (string)$binding, $sql, 1);
-                }
-                \Log::info($sql);
-                \Log::channel('sql')->info('[ROUTE]' . $route );
-                \Log::channel('sql')->info('[QUERY]' . $sql);
-                \Log::channel('sql')->info('[TIME]' . "time(ms):{$query->time}" . PHP_EOL);
-            });
+        if (in_array($this->app->environment(),['local', 'testing']) || $this->app->runningInConsole()) {
+            return;
         }
+
+        $logChannel = $this->app->environment('local')
+            ? 'sql'
+            : 'stack';
+
+        DB::listen(function ($query) use ($logChannel) {
+            $route =  'NOT URL';
+
+            if (!is_null(Request::route())) {
+                $route = Request::route()->getName();
+            }
+
+            $sql = $query->sql;
+            foreach ($query->bindings as $binding) {
+                if (is_string($binding)) {
+                    $binding = "'{$binding}'";
+                } elseif (is_bool($binding)) {
+                    $binding = $binding ? '1' : '0';
+                } elseif (is_int($binding)) {
+                    $binding = (string) $binding;
+                } elseif ($binding === null) {
+                    $binding = 'NULL';
+                } elseif ($binding instanceof Carbon) {
+                    $binding = "'{$binding->timezone(config('app.timezone'))->toDateTimeString()}'";
+                } elseif ($binding instanceof DateTime) {
+                    $binding = "'{$binding->setTimezone(new DateTimeZone(config('app.timezone')))->format('Y-m-d H:i:s')}'";
+                }
+                $sql = preg_replace('/\\?/', $binding, $sql, 1);
+            }
+            Log::info($sql);
+            Log::channel($logChannel)->info('[ROUTE]' . $route);
+            Log::channel($logChannel)->info('[QUERY]' . $sql);
+            Log::channel($logChannel)->info('[TIME]' . "time(ms):{$query->time}" . PHP_EOL);
+        });
     }
 }
