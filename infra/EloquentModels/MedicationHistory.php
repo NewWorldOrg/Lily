ae3a44ef3952e9145141cce9f8319a11643eec04
fyui001 <yun.cod@gmail.com>
2025-08-16 01:48:52 +0900

    add ide helper models

--- infra/EloquentModels/MedicationHistory.php
+++ infra/EloquentModels/MedicationHistory.php
@@ -13,6 +13,31 @@ use Domain\MedicationHistory\UserId;
 use Infra\EloquentModels\Model as AppModel;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $drug_id
+ * @property string $amount 服薬量
+ * @property string $medication_date
+ * @property \Illuminate\Support\Carbon|null $created_at 作成日時
+ * @property \Illuminate\Support\Carbon|null $updated_at 更新日時
+ * @property-read \Infra\EloquentModels\Drug $drug
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|MedicationHistory newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|MedicationHistory newQuery()
+ * @method static Builder<static>|MedicationHistory orWhereLike(string $attribute, string $keyword, int $position = 0)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|MedicationHistory query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|MedicationHistory whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|MedicationHistory whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|MedicationHistory whereDrugId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|MedicationHistory whereId($value)
+ * @method static Builder<static>|MedicationHistory whereLike(string $attribute, string $keyword, int $position = 0)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|MedicationHistory whereMedicationDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|MedicationHistory whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|MedicationHistory whereUserId($value)
+ * @mixin \Eloquent
+ */
 class MedicationHistory extends AppModel
 {
     protected $table = 'medication_histories';
